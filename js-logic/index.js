a6b6b66b439b638fc2b95ec740d2661be324f5a3
Nick Grunert <ngredblack@gmail.com>
2022-05-10 12:28:07 +0200

    Commit version 1 of identity

--- js-logic/index.js
+++ js-logic/index.js
@@ -1,9 +1,18 @@
 const Dash = require('dash');
+const fs = require('fs');
+
+const identity = require('./identity');
 /******************************************************************************/
 
 
 /******************************************************************************/
 const main = async () => {
+	var start;
+	var time;
+	var output;
+
+	
+	start = process.hrtime();
 	const client = new Dash.Client(
 		{
 			network: 'testnet',
@@ -15,21 +24,47 @@ const main = async () => {
 				},
 			},
 	});
+	//await client.platform.initialize();
+	time = process.hrtime(start);
+	fs.writeFile('./outputs', "Time -- Client: " + (time[0] + time[1]/1000000000) + "\n", { flag: 'a+' }, err => {});
+
+
+	start = process.hrtime();
+		await client.platform.identities.register()
+			.then((d) => output = d);
+	time = process.hrtime(start);
+	fs.writeFile('./outputs', "Time -- Register: " + (time[0] + time[1]/1000000000) + "\n", { flag: 'a+' }, err => {});
+	console.log(output.balance);
+
+	start = process.hrtime();
+		await client.platform.identities.register()
+			//.then((d) => output = d);
+	time = process.hrtime(start);
+	fs.writeFile('./outputs', "Time -- Register: " + (time[0] + time[1]/1000000000) + "\n", { flag: 'a+' }, err => {});
+	//console.log(output.balance);
+
+
+	start = process.hrtime();
+		await client.platform.identities.topUp(output.id, 0.0001);
+	time = process.hrtime(start);
+	fs.writeFile('./outputs', "Time -- Topup: " + (time[0] + time[1]/1000000000) + "\n", { flag: 'a+' }, err => {});
+
+
+	var newb;
+	start = process.hrtime();
+		await client.platform.identities.get(output.id)
+			.then((d) => newb = d.balance);
+	time = process.hrtime(start);
+	fs.writeFile('./outputs', "Time -- Get: " + (time[0] + time[1]/1000000000) + "\n", { flag: 'a+' }, err => {});
+	console.log(newb);
 	
 
-	var output;
-	await client.platform.identities.register()
-		.then((d) => output = d);
-		/*
-	var identity;
-	await client.platform.identities.get(id)
-		.then ((d) => identity = d);
-*/
-
-	console.log(output);
-
-
+	fs.writeFile('./outputs', "END OF TEST\n", { flag: 'a+' }, err => {});
 	client.disconnect();
 }
 
+
 main();
+
+
+module.exports = { main }
