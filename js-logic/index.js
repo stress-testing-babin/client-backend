361383f768ac7f6d5f42efdf3cbd6cd9fe5e3771
Nick Grunert <ngredblack@gmail.com>
2022-06-06 14:33:54 +0200

    Add first variant of code (outdated)

--- js-logic/index.js
+++ js-logic/index.js
@@ -33,6 +33,7 @@ const main = async () => {
 	try {
 		//get from data cache
 		let oldData = JSON.parse(fs.readFileSync('data.json'));
+		console.log(oldData.id)
 		let Identity;
 		if (oldData.id == "" || FORCE_NEWID) {
 			Identity = await identity.register(client)
@@ -40,9 +41,17 @@ const main = async () => {
 		} else {
 			Identity = await identity.get(client, oldData.id);
 		}
+
+
 		if (Identity.balance < 10000) await identity.topup(client, oldData.id, 1);
 		let contract_information = await contract.create(client, Identity, cname)
-		let document_info = await document.create(client, (cname + '.note'), Identity, { message: 'message' })
+
+		for (let i = 0; i < 10; ++i) {
+			document.create(client, (cname + '.note'), Identity, { message: 'message' })
+			if (Identity.balance < 10000) await identity.topup(client, oldData.id, 1);
+		}
+
+
 		//write "new" data to cache
 		fs.writeFileSync('data.json', JSON.stringify(oldData));
 		console.log("Saved data to data.json");
