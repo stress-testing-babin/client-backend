be1dd7ef6e8befbe348376abed9ac7f38fab71c8
Nick Grunert <ngredblack@gmail.com>
2022-06-12 22:28:49 +0200

    Upload cleanuped version

--- js-logic/index.js
+++ js-logic/index.js
@@ -17,23 +17,18 @@ const cleanup = () => {
 
 
 /******************************************************************************/
+//TODO search any data for dapiAddress:DAPIAddress {...}
+//var data = JSON.stringify({ 'id': id }, null, 2);
+//fs.writeFileSync('id.json', data);
+const FORCE_NEWID = false;	
+const cname = 'cname';
+/******************************************************************************/
 const main = async () => {
 	// not time sensitive, as it is preparational and takes milliseconds
 	const client = c.getClient();
-	//TODO search any data for dapiAddress:DAPIAddress {...}
-
-
-	//var data = JSON.stringify({ 'id': id }, null, 2);
-	//fs.writeFileSync('id.json', data);
-
-	const FORCE_NEWID = false;	
-	const cname = 'cname';
-
-
 	try {
 		//get from data cache
 		let oldData = JSON.parse(fs.readFileSync('data.json'));
-		console.log(oldData.id)
 		let Identity;
 		if (oldData.id == "" || FORCE_NEWID) {
 			Identity = await identity.register(client)
@@ -41,20 +36,18 @@ const main = async () => {
 		} else {
 			Identity = await identity.get(client, oldData.id);
 		}
-
-
+		/**********************************************************************/
+		//will never be needed
 		if (Identity.balance < 10000) await identity.topup(client, oldData.id, 1);
-		let contract_information = await contract.create(client, Identity, cname)
-
+		await contract.create(client, Identity, cname)
+		/**********************************************************************/
 		for (let i = 0; i < 10; ++i) {
 			document.create(client, (cname + '.note'), Identity, { message: 'message' })
 			if (Identity.balance < 10000) await identity.topup(client, oldData.id, 1);
 		}
-
-
+		/**********************************************************************/
 		//write "new" data to cache
 		fs.writeFileSync('data.json', JSON.stringify(oldData));
-		console.log("Saved data to data.json");
 	} catch (err) {
 		console.log(err);
 	} finally {
